a640df928b85ff0f16cfaf9e62a58020806eadf4
yongri-li <yongri.liii+1@gmail.com>
2021-09-14 19:18:32 -0400

    added user htmls

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('login/',views.login,name="login"),
+    path('logout/',views.logout,name="logout"),
+    path('register/',views.register,name="register"),
+    path('test/',views.test,name="test")
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,52 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django import forms
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(widget=forms.PasswordInput())
+
 
 def index(request):
     return render(request,"images/index.html")
+
+def login(request):
+
+    if(request.method == "POST"):
+        print("It was a post")
+        #is form vaild
+        #get data from form
+        #login
+        newUser = LoginForm(request.POST)
+        if newUser.is_valid():
+            username = newUser.cleaned_data["username"]
+            password = newUser.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            print(username)
+            print(password)
+
+
+    return render(request,"images/login.html",{
+        "form":LoginForm(),
+    })
+
+def logout(request):
+    print("You have been logged out")
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    return render(request,"images/register.html",{
+        "form":RegisterForm()
+    })
+
+def test(request):
+    return HttpResponse("Hello This is the Test View")
