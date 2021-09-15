81b045bde4e065bc046c6af2eccc4134139f3ab2
yongri-li <yongri.liii+1@gmail.com>
2021-09-14 20:28:43 -0400

    login register and logout working

--- images/urls.py
+++ images/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name="index"),
-    path('login/',views.login,name="login"),
-    path('logout/',views.logout,name="logout"),
-    path('register/',views.register,name="register"),
+    path('login/',views.login_view,name="login"),
+    path('logout/',views.logout_view,name="logout"),
+    path('register/',views.register_view,name="register"),
     path('test/',views.test,name="test")
 ]
--- images/views.py
+++ images/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
 from django.urls import reverse
+from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
+from .models import User
 # Create your views here.
 
 class LoginForm(forms.Form):
@@ -19,34 +21,60 @@ class RegisterForm(forms.Form):
 def index(request):
     return render(request,"images/index.html")
 
-def login(request):
+def login_view(request):
 
     if(request.method == "POST"):
-        print("It was a post")
-        #is form vaild
-        #get data from form
-        #login
+    
         newUser = LoginForm(request.POST)
         if newUser.is_valid():
             username = newUser.cleaned_data["username"]
             password = newUser.cleaned_data["password"]
             user = authenticate(request, username=username, password=password)
-            print(username)
-            print(password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, "images/login.html", {
+                    "form": LoginForm(),
+                    "message": "Invalid username and/or password."
+                })
+    else:
+        return render(request,"images/login.html",{
+            "form":LoginForm(),
+        })
 
-
-    return render(request,"images/login.html",{
-        "form":LoginForm(),
-    })
-
-def logout(request):
-    print("You have been logged out")
+def logout_view(request):
+    logout(request)
     return HttpResponseRedirect(reverse("index"))
 
-def register(request):
-    return render(request,"images/register.html",{
-        "form":RegisterForm()
-    })
+def register_view(request):
+    if(request.method == "POST"):
+        newUser = RegisterForm(request.POST)
+
+        if newUser.is_valid():
+            username = newUser.cleaned_data["username"]
+            password = newUser.cleaned_data["password"]
+            passwordCom = newUser.cleaned_data["password_confirm"]
+            userEmail = newUser.cleaned_data["email"]
+            if password != passwordCom:
+                return render(request, "images/register.html", {
+                    "message": "Passwords did not match.",
+                    "form":RegisterForm()
+                })
+            try:
+                user = User.objects.create_user(username, userEmail, password)
+                user.save()
+            except IntegrityError:
+                return render(request, "images/register.html", {
+                    "message": "Username already taken.",
+                    "form":RegisterForm()
+                })
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request,"images/register.html",{
+            "form":RegisterForm()
+        })
 
 def test(request):
     return HttpResponse("Hello This is the Test View")
