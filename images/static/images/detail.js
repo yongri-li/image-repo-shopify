a76ba12d70ef24c7e2697d341c978f467bc5e9f3
yongri-li <yongri.liii+1@gmail.com>
2021-09-17 00:43:00 -0400

    expanded repo detail

--- images/static/images/detail.js
+++ images/static/images/detail.js
@@ -1,17 +1,62 @@
 
 
 console.log("HELLO")
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-    const imageList = document.querySelector('#image-list')
-    const repoID = document.URL.split("/")[document.URL.split("/").length-1]
-    
+    const repo = document.querySelector('#repo-detail')
+    const repoID = document.URL.split("/")[document.URL.split("/").length - 1]
+
     fetch(`/repo_details/${repoID}`)
-    .then(response => response.json())
-    .then(details => {
-        console.log(details)
-    })
-    
+        .then(response => response.json())
+        .then(details => {
+            console.log(details);
+            let title = document.createElement("h2");
+            let user = document.createElement("h5");
+            let selected = document.createElement("button");
+            selected.addEventListener('click', function () {
+                console.log("BUTTON PRESSED");
+
+            })
+
+
+            selected.innerHTML = `Delete Selected`
+            user.innerHTML = `created by ${details.repo.author}`;
+            title.innerHTML = details.repo.title;
+
+            repo.append(title);
+            repo.append(user);
+            repo.append(selected);
+
+            for (let i = 0; i < details.images.length; i++) {
+
+
+                let newContent = document.createElement("div");
+                newContent.style.borderStyle = "groove"
+                newContent.style.margin = "10px"
+
+                let check = document.createElement("input")
+                check.setAttribute("type", "checkbox")
+
+                let title = document.createElement("h3");
+                title.innerHTML = details.images[i].title
+
+                let newImage = document.createElement("img");
+                newImage.setAttribute("src", details.images[i].image);
+                newImage.setAttribute("height", "100");
+
+
+                newContent.append(title);
+                newContent.append(check);
+                newContent.append(newImage);
+
+                repo.append(newContent);
+            }
+
+
+
+
+        })
+
     //console.log("HEHLDFDF")
     //fetch(`/detail/${repoID}`)
 
--- images/static/images/home.js
+++ images/static/images/home.js
@@ -1,38 +1,40 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     //GET a list of images
     const repoList = document.querySelector('#repo-list')
 
     fetch(`/images/private`)
-    .then(response => response.json())
-    .then(repos => {
-        console.log(repos)
-        for (let i =0;i<repos.length;i++) {
-            let newContent = document.createElement('div');
-            newContent.style.borderStyle = "groove"
-            let theImage = document.createElement('img');
-            let title = document.createElement("p");
-            let des = document.createElement("p");
+        .then(response => response.json())
+        .then(repos => {
+            console.log(repos)
+            for (let i = 0; i < repos.length; i++) {
+                let newContent = document.createElement('div');
+                newContent.style.borderStyle = "groove"
 
-            title.innerHTML = repos[i].title
+                newContent.style.margin = "10px"
+                let theImage = document.createElement('img');
+                let title = document.createElement("h3");
+                let des = document.createElement("p");
 
-            des.innerText = repos[i].description
+                title.innerHTML = repos[i].title;
 
-            theImage.setAttribute("src",repos[i].thumbnail);
-            theImage.setAttribute("alt",repos[i].title)
-            theImage.setAttribute("height","100")
+                des.innerText = repos[i].description;
 
-            newContent.append(title)
-            newContent.append(theImage)
-            newContent.append(des)
-            newContent.addEventListener('click', () => {
-                console.log(`YOU JUST CLICKED ${repos[i].title} with the id of ${repos[i].id}`)
-                window.location.href = `/detail/${repos[i].id}`;
-            })
+                theImage.setAttribute("src", repos[i].thumbnail);
+                theImage.setAttribute("alt", repos[i].title);
+                theImage.setAttribute("height", "100");
+
+                newContent.append(title);
+                newContent.append(theImage);
+                newContent.append(des);
+                newContent.addEventListener('click', () => {
+                    console.log(`YOU JUST CLICKED ${repos[i].title} with the id of ${repos[i].id}`);
+                    window.location.href = `/detail/${repos[i].id}`;
+                })
 
 
-            repoList.append(newContent)
-        }
-    })
+                repoList.append(newContent);
+            }
+        })
 })
